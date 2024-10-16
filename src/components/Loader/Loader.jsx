ba638f2db3261a7f240c65b95b3585fe20ab4f52
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-16 13:21:22 +0300

    Add HomePage markup and Loader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,20 @@
+import { LineWave } from 'react-loader-spinner';
+
+const Loader = () => {
+  return (
+    <LineWave
+      visible={true}
+      height="100"
+      width="100"
+      color="#4fa94d"
+      ariaLabel="line-wave-loading"
+      wrapperStyle={{}}
+      wrapperClass=""
+      firstLineColor=""
+      middleLineColor=""
+      lastLineColor=""
+    />
+  );
+};
+
+export default Loader;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from 'react';
+import fetchTrendingMovies from '../tmdb-api';
+import MovieList from '../components/MovieList/MovieList';
+import Loader from '../components/Loader/Loader';
+
 const HomePage = () => {
-  return <h1>Trending today</h1>;
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const loadMovies = async () => {
+      setLoading(true);
+      try {
+        const trendingMovies = await fetchTrendingMovies();
+        setMovies(trendingMovies);
+      } catch (err) {
+        setError(`Failed to load movies: ${err.message}`);
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadMovies();
+  }, []);
+
+  if (loading) return <Loader />;
+  if (error) return <h2>{error}</h2>;
+
+  return (
+    <div>
+      <h1>Trending today</h1>
+      <MovieList movies={movies} />
+    </div>
+  );
 };
 
 export default HomePage;
