b9099e162dc09052d154b9077fad5f483c4ac811
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-17 19:06:11 +0300

    Add MovieReviews component

--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -1,5 +1,43 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Loader from '../Loader/Loader';
+import css from './MovieReviews.module.css';
+
 const MovieReviews = () => {
-  return <div></div>;
+  const { movieId } = useParams();
+  const [reviews, setReviews] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const loadMovieReviews = async () => {
+      setLoading(true);
+      try {
+        const reviewsData = await fetchMovieReviews(movieId);
+        setReviews(reviewsData);
+      } catch {
+        setError('Failed to load reviews');
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadMovieReviews();
+  }, [movieId]);
+
+  if (loading) return <Loader />;
+  if (error) return <h2>{error}</h2>;
+  if (reviews.length === 0) return <p>No reviews available for this movie.</p>;
+
+  return (
+    <ul className={css.reviewsList}>
+      {reviews.map(({ id, author, content }) => (
+        <li key={id} className={css.reviewItem}>
+          <h3>Review by {author}</h3>
+          <p>{content}</p>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default MovieReviews;
