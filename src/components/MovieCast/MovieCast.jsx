958f42a19d9999a7cdbafcef8d6bf1652f10f3b6
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-17 15:29:50 +0300

    Add MovieCast component

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -1,5 +1,53 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Loader from '../Loader/Loader';
+import css from './MovieCast.module.css';
+
 const MovieCast = () => {
-  return <div></div>;
+  const { movieId } = useParams();
+  const [cast, setCast] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const loadMovieCast = async () => {
+      setLoading(true);
+      try {
+        const castData = await fetchMovieCast(movieId);
+        setCast(castData);
+      } catch {
+        setError('Failed to load cast information');
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadMovieCast();
+  }, [movieId]);
+
+  if (loading) return <Loader />;
+  if (error) return <h2>{error}</h2>;
+  if (!cast) return <p>No cast information available.</p>;
+
+  return (
+    <ul className={css.castList}>
+      {cast.map(({ id, name, profile_path, character }) => (
+        <li key={id} className={css.castItem}>
+          <img
+            src={
+              profile_path
+                ? `https://image.tmdb.org/t/p/w200${profile_path}`
+                : '/default-profile.jpg'
+            }
+            alt={name}
+            className={css.castImage}
+          />{' '}
+          <p>
+            {name} as {character}
+          </p>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default MovieCast;
