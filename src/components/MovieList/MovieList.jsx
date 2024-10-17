1282f356bcd0e44d5b4c73d8eee99e8f8b50269e
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-17 20:38:52 +0300

    Add poster

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,10 +3,20 @@ import css from './MovieList.module.module.css';
 
 const MovieList = ({ movies }) => {
   return (
-    <ul className={css.list}>
-      {movies.map(movie => (
-        <li key={movie.id} className={css.item}>
-          <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+    <ul className={css.movieList}>
+      {movies.map(({ id, title, poster_path }) => (
+        <li key={id} className={css.movieItem}>
+          <Link to={`/movies/${id}`}>
+            <img
+              src={
+                poster_path
+                  ? `https://image.tmdb.org/t/p/w200${poster_path}`
+                  : 'path-to-placeholder-image.jpg'
+              }
+              alt={title}
+            />
+            <p className={css.movieTitle}>{title}</p>
+          </Link>
         </li>
       ))}
     </ul>
