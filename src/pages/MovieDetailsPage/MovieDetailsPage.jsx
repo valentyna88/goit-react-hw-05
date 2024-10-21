7bc51c7e42aa41bb079d33d9b1e193f83fa50815
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-21 15:25:35 +0300

    Add MovieDetailsPage styles

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -4,6 +4,7 @@ import Loader from '../../components/Loader/Loader';
 import { fetchMovieDetails } from '../../api';
 import GoBack from '../../components/GoBack/GoBack';
 import css from './MovieDetailsPage.module.css';
+import clsx from 'clsx';
 
 const MovieDetailsPage = () => {
   const { movieId } = useParams();
@@ -35,10 +36,15 @@ const MovieDetailsPage = () => {
   const { title, poster_path, overview, vote_average, genres, release_date } =
     movie;
 
+  const buildLinkClass = ({ isActive }) => {
+    return clsx(css.link, isActive && css.active);
+  };
+
   return (
-    <main className={css.card}>
+    <main className={css.container}>
       <GoBack to={backLinkHref}>Go back</GoBack>
-      <div className={css.cardWrapper}>
+      <h2 className={css.title}>{title}</h2>
+      <section className={css.cardWrapper}>
         <img
           className={css.poster}
           src={
@@ -49,50 +55,55 @@ const MovieDetailsPage = () => {
           alt={title}
         />
         <div className={css.infoWrapper}>
-          <h2>{title}</h2>
-          <div>
-            <h3>Release Year: </h3>
-            <p>{release_date ? release_date.split('-')[0] : 'N/A'}</p>
-          </div>
-          <div>
-            <h3>User Score: </h3>
-            <p>{vote_average ? `${vote_average * 10}%` : 'N/A'}</p>
-          </div>
-          <div>
-            <h3>Overview: </h3>
-            <p>{overview}</p>
-          </div>
-          <div>
-            <h3>Rating: </h3>
-            <p>{vote_average}</p>
-          </div>
-          <div>
-            <h3>Genres: </h3>
-            <ul>
-              {genres && genres.length > 0 ? (
-                genres.map(genre => <li key={genre.id}>{genre.name}</li>)
-              ) : (
-                <li>No genres available</li>
-              )}
-            </ul>
-          </div>
+          <h3 className={css.subtitle}>Release Year: </h3>
+          <p className={css.text}>
+            {release_date ? release_date.split('-')[0] : ''}
+          </p>
+
+          <h3 className={css.subtitle}>User Score: </h3>
+          <p className={css.text}>
+            {vote_average ? `${vote_average * 10}%` : '0'}
+          </p>
+
+          <h3 className={css.subtitle}>Overview: </h3>
+          <p className={css.text}>{overview}</p>
+
+          <h3 className={css.subtitle}>Genres: </h3>
+          <ul className={css.list}>
+            {genres && genres.length > 0 ? (
+              genres.map(genre => (
+                <li className={css.item} key={genre.id}>
+                  {genre.name}
+                </li>
+              ))
+            ) : (
+              <li className={css.item}>No genres available</li>
+            )}
+          </ul>
         </div>
-      </div>
-      <div className={css.links}>
-        <p>Additional information</p>
-        <ul>
-          <li>
-            <NavLink to="cast" state={{ ...location.state }}>
-              Cast
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="reviews" state={{ ...location.state }}>
-              Reviews
-            </NavLink>
-          </li>
-        </ul>
-      </div>
+      </section>
+
+      <ul className={css.infoLinks}>
+        <li>
+          <NavLink
+            to="cast"
+            className={buildLinkClass}
+            state={{ ...location.state }}
+          >
+            Cast
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="reviews"
+            className={buildLinkClass}
+            state={{ ...location.state }}
+          >
+            Reviews
+          </NavLink>
+        </li>
+      </ul>
+
       <Outlet />
     </main>
   );
