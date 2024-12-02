011816a1d8dd581a87eb999b227b01c7d9ca8165
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-12-02 23:23:28 +0200

    Add MovieDetails component

---
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -0,0 +1,100 @@
+import { FaRegCommentDots } from 'react-icons/fa';
+import css from './MovieDetails.module.css';
+
+const MovieDetails = ({ movie, reviewCount }) => {
+  const {
+    title,
+    poster_path,
+    overview,
+    vote_average,
+    genres,
+    release_date,
+    runtime,
+    production_countries,
+  } = movie;
+
+  return (
+    <>
+      <div className={css.titleBlock}>
+        <h2 className={css.title}>{title}</h2>
+      </div>
+      <section className={css.cardWrapper}>
+        <img
+          className={css.poster}
+          src={
+            poster_path
+              ? `https://image.tmdb.org/t/p/w500${poster_path}`
+              : 'path-to-placeholder-image.jpg'
+          }
+          alt={title}
+        />
+        <div className={css.infoWrapper}>
+          <div className={css.infoItem}>
+            <h3 className={css.subtitle}>Release Year: </h3>
+            <span className={css.dashedLine}></span>
+            <p className={css.text}>
+              {release_date ? release_date.split('-')[0] : ''}
+            </p>
+          </div>
+
+          <div className={css.infoItem}>
+            <h3 className={css.subtitle}>Production:</h3>
+            <span className={css.dashedLine}></span>
+            <p className={css.text}>
+              {production_countries && production_countries.length > 0
+                ? production_countries.map(country => country.name).join(', ')
+                : 'No information available'}
+            </p>
+          </div>
+
+          <div className={css.infoItem}>
+            <h3 className={css.subtitle}>Genres: </h3>
+            <span className={css.dashedLine}></span>
+            <ul className={css.list}>
+              {genres && genres.length > 0 ? (
+                genres.map(genre => (
+                  <li className={css.item} key={genre.id}>
+                    {genre.name}
+                  </li>
+                ))
+              ) : (
+                <li className={css.item}>No genres available</li>
+              )}
+            </ul>
+          </div>
+
+          <div className={css.infoItem}>
+            <h3 className={css.subtitle}>Runtime:</h3>
+            <span className={css.dashedLine}></span>
+            <p className={css.text}>{runtime} min</p>
+          </div>
+
+          <div className={css.infoItem}>
+            <h3 className={css.subtitle}>User Score: </h3>
+            <span className={css.dashedLine}></span>
+            <p className={css.text}>
+              {vote_average ? `${(vote_average * 10).toFixed(2)}%` : '0'}
+            </p>
+          </div>
+
+          <div className={css.infoItem}>
+            <h3 className={css.subtitle}>Overview: </h3>
+            <span className={css.dashedLine}></span>
+            <p className={css.text}>{overview}</p>
+          </div>
+
+          <div className={css.commentsBlock}>
+            <FaRegCommentDots className={css.commentIcon} />
+            <span>
+              {reviewCount > 0
+                ? `${reviewCount} Review${reviewCount > 1 ? 's' : ''}`
+                : 'No reviews yet'}
+            </span>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default MovieDetails;
--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -6,7 +6,7 @@ import GoBack from '../../components/GoBack/GoBack';
 import css from './MovieDetailsPage.module.css';
 import clsx from 'clsx';
 import DocumentTitle from '../../components/DocumentTitle';
-import { FaRegCommentDots } from 'react-icons/fa';
+import MovieDetails from '../../components/MovieDetails/MovieDetails';
 
 const MovieDetailsPage = () => {
   const { movieId } = useParams();
@@ -42,17 +42,6 @@ const MovieDetailsPage = () => {
   if (error) return <h2>{error}</h2>;
   if (!movie) return null;
 
-  const {
-    title,
-    poster_path,
-    overview,
-    vote_average,
-    genres,
-    release_date,
-    runtime,
-    production_countries,
-  } = movie;
-
   const buildLinkClass = ({ isActive }) => {
     return clsx(css.link, isActive && css.active);
   };
@@ -62,84 +51,8 @@ const MovieDetailsPage = () => {
       <DocumentTitle>MovieDetails</DocumentTitle>
       <main className={css.container}>
         <GoBack to={backLinkHref}>Go back</GoBack>
-        <div className={css.titleBlock}>
-          <h2 className={css.title}>{title}</h2>
-        </div>
-        <section className={css.cardWrapper}>
-          <img
-            className={css.poster}
-            src={
-              poster_path
-                ? `https://image.tmdb.org/t/p/w500${poster_path}`
-                : 'path-to-placeholder-image.jpg'
-            }
-            alt={title}
-          />
-          <div className={css.infoWrapper}>
-            <div className={css.infoItem}>
-              <h3 className={css.subtitle}>Release Year: </h3>
-              <span className={css.dashedLine}></span>
-              <p className={css.text}>
-                {release_date ? release_date.split('-')[0] : ''}
-              </p>
-            </div>
 
-            <div className={css.infoItem}>
-              <h3 className={css.subtitle}>Production:</h3>
-              <span className={css.dashedLine}></span>
-              <p className={css.text}>
-                {production_countries && production_countries.length > 0
-                  ? production_countries.map(country => country.name).join(', ')
-                  : 'No information available'}
-              </p>
-            </div>
-
-            <div className={css.infoItem}>
-              <h3 className={css.subtitle}>Genres: </h3>
-              <span className={css.dashedLine}></span>
-              <ul className={css.list}>
-                {genres && genres.length > 0 ? (
-                  genres.map(genre => (
-                    <li className={css.item} key={genre.id}>
-                      {genre.name}
-                    </li>
-                  ))
-                ) : (
-                  <li className={css.item}>No genres available</li>
-                )}
-              </ul>
-            </div>
-
-            <div className={css.infoItem}>
-              <h3 className={css.subtitle}>Runtime:</h3>
-              <span className={css.dashedLine}></span>
-              <p className={css.text}>{runtime} min</p>
-            </div>
-
-            <div className={css.infoItem}>
-              <h3 className={css.subtitle}>User Score: </h3>
-              <span className={css.dashedLine}></span>
-              <p className={css.text}>
-                {vote_average ? `${(vote_average * 10).toFixed(2)}%` : '0'}
-              </p>
-            </div>
-
-            <div className={css.infoItem}>
-              <h3 className={css.subtitle}>Overview: </h3>
-              <span className={css.dashedLine}></span>
-              <p className={css.text}>{overview}</p>
-            </div>
-
-            <div className={css.commentsBlock}>
-              <FaRegCommentDots className={css.commentIcon} />
-              <span>
-                {reviewCount > 0
-                  ? `${reviewCount} Review${reviewCount > 1 ? 's' : ''}`
-                  : 'No reviews yet'}
-              </span>
-            </div>
-          </div>
-        </section>
+        <MovieDetails movie={movie} reviewCount={reviewCount} />
 
         <ul className={css.infoLinks}>
           <li>
